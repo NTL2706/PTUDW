3c5f4e1f9c489a47adb62419a928db5c6aeecff8
Nguyễn Tiến Lợi <92145801+NTL2706@users.noreply.github.com>
2024-01-13 16:08:46 +0700

    === Create === create api report

--- server/src/routers/api/index.js
+++ server/src/routers/api/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { placeAdsAPI } from "../../controllers/apiController.js";
+import { placeAdsAPI, reportAPI } from "../../controllers/apiController.js";
 
 const route = Router();
 
 route.get("/placeAds", placeAdsAPI);
+route.post("/report", reportAPI);
 
 export default route;
